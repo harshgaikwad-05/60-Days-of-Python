8d6c065037573e16b5c8357013461f2e66ccd010
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-01-22 23:44:30 +0530

    Everything about Python

---
+++ Day - 1/Tut - 2.py
@@ -0,0 +1,5 @@
+print("Python is a popular, high-level, general-purpose programming language known for its simple, readable syntax that emphasizes code clarity and allows developers to use fewer lines of code than many other languages. Created by Guido van Rossum and first released in 1991, its name comes from the British comedy series Monty Python's Flying Circus.")
+
+print("Python is very powerful language we can use it for the AI/ML and many more powerful applications and for research ")
+
+print("We will learn everything about the python in everyday life and the consisency will not break. I am just following one rule that's Learning slow but learning with sonsistency.")
