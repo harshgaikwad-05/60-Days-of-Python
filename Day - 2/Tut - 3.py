17be7501fa58cc53f7f93842361ba30ca238e06a
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-01-24 01:28:47 +0530

    Tutorial on Python modules and pip

---
+++ Day - 2/Tut - 3.py
@@ -0,0 +1,66 @@
+# Pips and modules in python
+
+# Concept     	Meaning
+
+# Module      	A Python file with code
+# Package     	A collection of modules
+# pip         	Tool to install packages
+# import	    Used to use a module
+
+# What is a Module in Python?
+# Think of a module like a toolbox
+# Instead of writing everything from scratch, Python lets you reuse code that’s already written.
+
+# A module is simply a file that contains Python code:
+# functions
+# variables
+# classes
+# You can import that file and use what’s inside it.
+
+# 📦 Why modules exist
+# Without modules:
+# Your code would be very long
+# You’d repeat the same logic again and again
+
+# With modules:
+# Code is organized
+# Easy to reuse
+# Easy to read and maintain
+
+import math
+
+print(math.sqrt(16))   # 4.0
+print(math.pi)         # 3.141592653589793
+
+# External / third-party modules
+# These are not included with Python.
+# Examples:
+# numpy
+# pandas
+# requests
+# flask
+
+# 👉 This is where pip comes in.
+
+# What is pip in Python?
+# pip = Python Package Installer
+
+# Think of pip as:
+# 🛒 An app store for Python libraries
+
+# It lets you:
+# Download
+# Install
+# Update
+# Remove Python packages (modules)
+
+# 📦 What is a package?
+# A package is a collection of modules
+# Often very powerful and large
+
+# Example:
+# numpy → math & arrays
+# pandas → data analysis
+# requests → HTTP requests
+
+# Will understand about pips and modules in detail while working on projects.
