5e4add0563f235cdc35e943fe82517c2aa5775fe
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-01-24 01:36:47 +0530

    Initial Python tutorial script Hello World

---
+++ Day - 2/Tut - 4.py
@@ -0,0 +1,5 @@
+# The first Program In Python
+
+print("Hello World")    # For the strings we have to use the "Double Quotes" or 'Single Quote' and it is compulsory to use it.
+print(5)                # Integer is denoted directly
+print("Byee")
