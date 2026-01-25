d80811db311f11026d39d6a711ac0978ca4a931a
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-01-26 01:40:08 +0530

    Tutorial on comments, escape sequences, and print statements

---
+++ Day - 3/Tut - 5.py
@@ -0,0 +1,23 @@
+# In this we will learn about the comments, Escape Sequence and Print statements.
+
+# The comments are used to define a function or what the program or logic does
+
+# | Escape Sequence | Meaning         |
+# | --------------- | --------------- |
+# | `\n`            | New line        |
+# | `\t`            | Tab space       |
+# | `\\`            | Backslash       |
+# | `\"`            | Double quote    |
+# | `\'`            | Single quote    |
+# | `\r`            | Carriage return |
+# | `\b`            | Backspace       |
+
+# An escape sequence is a special character used inside strings that starts with a backslash \. It tells Python to do something special
+
+print("print is a built-in function in Python used to show output on the screen.")
+
+# Important rules to remember ⚠️
+# Text → inside quotes or Double Quotes
+# Numbers → no quotes
+# Use comma, not + (for now)
+# print always shows output on screen
