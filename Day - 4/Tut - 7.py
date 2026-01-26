d16b31dbcdfcf591845be4ddc576cc554149ae1a
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-01-26 20:07:41 +0530

    Tutorial on basic operators and calculator implementation

---
+++ Day - 4/Tut - 7.py
@@ -0,0 +1,32 @@
+# Learning about the Basic operators
+# Small Quiz = Calculator
+
+# Types of Operators in python
+
+# 1. Arithmetic Operators
+# | Operator | Meaning             | Example         |
+# | -------- | ------------------- | --------------- |
+# | `+`      | Addition            | `3 + 2` → `5`   |
+# | `-`      | Subtraction         | `5 - 2` → `3`   |
+# | `*`      | Multiplication      | `4 * 2` → `8`   |
+# | `/`      | Division            | `5 / 2` → `2.5` |
+# | `%`      | Modulus (remainder) | `5 % 2` → `1`   |
+# | `**`     | Exponent            | `2 ** 3` → `8`  |
+# | `//`     | Floor division      | `5 // 2` → `2`  |
+
+# Creating a Calculator
+a = 10
+b = 20
+print("Te addition of", a, "and",b,"is", a+b)
+print("The subtraction of", a, "and",b,"is", a-b)
+print("The multiplication of", a, "and",b,"is", a*b)
+print("The division of", a, "and",b,"is", a/b)
+print("The modulus of", a, "and",b,"is", a%b)
+print("The exponent of", a, "and",b,"is", a**b)
+print("The floor division of", a, "and",b,"is", a//b)
+
+# | Operation      | Operator | Result | Meaning                  |
+# | -------------- | -------- | ------ | ------------------------ |
+# | Division       | `/`      | `3.33` | Exact quotient           |
+# | Modulo         | `%`      | `1`    | Remainder                |
+# | Floor Division | `//`     | `3`    | Quotient without decimal |
