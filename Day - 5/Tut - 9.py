6e5e8b2f1144a121a9d773a6cc4307b6139d12e8
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-01-27 23:38:51 +0530

    Tutorial for taking user input

---
+++ Day - 5/Tut - 9.py
@@ -0,0 +1,14 @@
+# Taking user input in python
+# "input()" function is used for taking the input from the user
+
+name = (input("Enter Your name : "))     # The input function by default fetches the str(String) value 
+age = int(input("Enter Your age: "))     # To input the integeger we have to use the Type casting 
+
+print("Hello", name, "Good Morning")
+print("Your age is ",age)
+
+salary = float(input("Enter Your Salary"))
+work_experience = (input("Enter Your work experience"))
+
+print("Your Current salary is: ",salary)
+print("Your work experience is: ",work_experience)
